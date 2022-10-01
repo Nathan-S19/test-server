57114ded28de57d84847f3987588d1b6408b9a73
Nathan-S19 <nathan.sass@hotmail.com>
2022-10-01 23:18:51 +1000

    add about page

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
 
-	res.send('Hello World!');
-
+app.get('/about', (req, res) => {
+  res.send('About Page');
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-
